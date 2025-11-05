b9558a3dad0e9fc0a26b37c251e1c888147b9fe3
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-05 18:06:35 -0300

    pasamos por props y mostramos en las cards las citas ingresadas

--- src/components/ItemCita.jsx
+++ src/components/ItemCita.jsx
@@ -3,24 +3,26 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-const ItemCita = () => {
+const ItemCita = ({ cita }) => {
     return (
         <Col>
             <Card >
                 <Card.Header>
                     {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                    <p>Mascota:</p>
-                    <p>Dueño:</p>
+                    <ul class="list-unstyled">
+                        <li>Mascota: {cita.nombreMascota}</li>
+                        <li>Dueño:   {cita.nombreDueno}</li>
+                    </ul>
                 </Card.Header>
                 <Card.Body className="bg-secondary-subtle rounded shadow">
-                    <div>
-                        <p>Fecha:</p>
-                        <p>Hora:</p>
-                        <p>Sintomas:</p>
-                    </div>
+                    <ul class="list-unstyled">
+                        <li>Fecha:   {cita.fechaCita}</li>
+                        <li>Hora:    {cita.horaCita}</li>
+                        <li>Sintomas:{cita.sintomas}</li>
+                    </ul>
                 </Card.Body>
                 <Card.Footer className='d-flex justify-content-center'>
-                    <Button variant="danger">Borrar</Button>
+                    <Button variant="danger my-2">Borrar</Button>
                 </Card.Footer>
             </Card>
         </Col>
--- src/components/ListaCitas.jsx
+++ src/components/ListaCitas.jsx
@@ -4,11 +4,12 @@ import ItemCita from "./ItemCita";
 const ListaCitas = ({ arrayCitas }) => {
     return (
         <>
-            <section className="bg-secondary-subtle rounded shadow">
-                <div className="text-center">
+            <section className=" rounded shadow pb-3">
+                <div className="text-center mt-5">
                     <p className="text-bg-secondary py-2">Lista de Citas</p>
                 </div>
-                <Row xs={1} md={2} className="container g-3">
+
+                <Row xs={1} md={2} className="g-3">
                     {
                         arrayCitas.map((cita, indice) => <ItemCita key={indice} cita={cita} ></ItemCita>)
                     }
